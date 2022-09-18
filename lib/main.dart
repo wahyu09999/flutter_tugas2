0a12b6902daf8b4c4d121cd239ccf667b18d2b80
=wahyu09999 <=2041720191@student.polinema.ac.id>
2022-09-18 19:59:34 +0700

    Layout 2

--- lib/main.dart
+++ lib/main.dart
@@ -26,17 +26,15 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class Navbar extends StatelessWidget {
+  const Navbar({
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(   
-        title: Text(widget.title),
-      ),
-      body: 
-        ListView(
-          children: <Widget>[
-            Container(
+    ///Navbar
+            return Container(
               padding: const EdgeInsets.all(3.5),
               margin: const EdgeInsets.all(15),
               height: 20.5,
@@ -48,48 +46,22 @@ class _MyHomePageState extends State<MyHomePage> {
                   Text("PERTANDINGAN HARI INI"),
                   ],
                 ),
-            ),
-            
-            Container(
-              margin: const EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.purple)
-              ),
-              child: 
-              Column(
-                children: <Widget>[
-                  Container(         
-                    height: 150,          
-                    decoration: const BoxDecoration(               
-                        image: DecorationImage(
-                          alignment: Alignment.topCenter,
-                           fit: BoxFit.fitWidth,
-                          image: AssetImage('assets/images/bola.jpeg'),  
-                        ),
-                    ),
-                  ),
+            );
+  }
+}
 
-                  Container(
-                    margin: const EdgeInsets.all(10.0),
-                    child: const Text("Costa Mendekat ke Palmeiras",
-                    style: TextStyle(
-                      fontSize:20,
-                          color:Colors.black,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ), 
 
-                  Container(
-                  padding: const EdgeInsets.all(20.0),
-                  alignment: Alignment.centerLeft,
-                  color: Colors.purpleAccent,
-                  child: const Text("Transfer"),
-                  ),
-                ],
-              ),
-            ),
 
-            Container(
+class BeritaKecil extends StatelessWidget{
+  const BeritaKecil({
+
+    Key? key,
+  }) : super (key: key);
+
+   @override
+  Widget build(BuildContext context) {
+
+  return Container(
               margin: const EdgeInsets.only(left: 10.0, right: 10.0,),
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.grey)
@@ -142,175 +114,199 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ],
               ),
-            ),
+            );
+}
+}
 
-            Container(
-              margin: const EdgeInsets.only(left: 10.0, right: 10.0,top: 10),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey)
-              ),
-              child: 
-              Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Container(
-                        width: 170.7,
-                        height: 100, 
-                        decoration: BoxDecoration(  
-                          border: Border.all(color: Colors.grey),             
-                          image: const DecorationImage(
-                            alignment: Alignment.center,
-                            fit: BoxFit.fitWidth,
-                            image: AssetImage('assets/images/1.jpeg'),  
-                          ),                    
-                        ),
-                      ),
+class BeritaUtama extends StatelessWidget {
+  const BeritaUtama({
+    Key? key,
+  }) : super(key: key);
 
-                      Container(
-                        padding: const EdgeInsets.all(5.0),
-                        width: 167.3,
-                        height: 100, 
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey),
-                        ),
-                        child: const Text("Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat", 
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 11)
-                          ),
-                      ),
-                    ],
-                  ),
-
-                  Container(
-                    height: 30,
-                    alignment: Alignment.centerLeft,
-                    width: double.infinity,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey),
-                        ),
-                        child: const Text("Barcelona Feb 13, 2021",
-                        style: TextStyle(fontSize: 11)
-                        ),
-                  ),
-                ],
-              ),
-            ),
-
-            Container(
-              margin: const EdgeInsets.only(left: 10.0, right: 10.0,top: 10),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey)
-              ),
-              child: 
-              Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Container(
-                        width: 170.7,
-                        height: 100, 
-                        decoration: BoxDecoration(  
-                          border: Border.all(color: Colors.grey),             
-                            image: const DecorationImage(
-                              alignment: Alignment.center,
-                              fit: BoxFit.fitWidth,
-                              image: AssetImage('assets/images/1.jpeg'),  
-                            ),                    
-                        ),
-                      ),
-
-                         Container(
-                           padding: const EdgeInsets.all(5.0),
-                          width: 167.3,
-                          height: 100, 
-                          alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                  ),
-                        child: const Text("Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat", 
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 11)
-                        ),
-                      ),
-                      
-                    ],
-                    
-                  ),
-
-                  Container(
-                    height: 30,
-                    alignment: Alignment.centerLeft,
-                    width: double.infinity,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                      ),
-                    child: const Text("Barcelona Feb 13, 2021",
-                    style: TextStyle(fontSize: 11)
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            Container(
+  @override
+  Widget build(BuildContext context) {
+     return Container(
               margin: const EdgeInsets.all(10.0),
               decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey)
+                border: Border.all(color: Colors.purple)
               ),
               child: 
               Column(
                 children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Container(
-                        width: 170.7,
-                        height: 100, 
-                        decoration: BoxDecoration(  
-                          border: Border.all(color: Colors.grey),             
-                            image: const DecorationImage(
-                              alignment: Alignment.center,
-                              fit: BoxFit.fitWidth,
-                              image: AssetImage('assets/images/1.jpeg'),  
-                            ),                    
+                  Container(         
+                    height: 150,          
+                    decoration: const BoxDecoration(               
+                        image: DecorationImage(
+                          alignment: Alignment.topCenter,
+                           fit: BoxFit.fitWidth,
+                          image: AssetImage('assets/images/bola.jpeg'),  
                         ),
-                      ),
-
-                         Container(
-                           padding: const EdgeInsets.all(5.0),
-                          width: 167.3,
-                          height: 100, 
-                          alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                  ),
-                        child: const Text("Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat", 
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 11)
-                        ),
-                      ),
-                      
-                    ],
-                    
+                    ),
                   ),
 
                   Container(
-                    height: 30,
-                    alignment: Alignment.centerLeft,
-                    width: double.infinity,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                      ),
-                    child: const Text("Barcelona Feb 13, 2021",
-                    style: TextStyle(fontSize: 11)
+                    margin: const EdgeInsets.all(10.0),
+                    child: const Text("Costa Mendekat ke Palmeiras",
+                    style: TextStyle(
+                      fontSize:20,
+                          color:Colors.black,
+                          fontWeight: FontWeight.bold),
                     ),
+                  ), 
+
+                  Container(
+                  padding: const EdgeInsets.all(20.0),
+                  alignment: Alignment.centerLeft,
+                  color: Colors.purpleAccent,
+                  child: const Text("Transfer"),
                   ),
                 ],
               ),
-            ),
-          ],
+            );
+  }
+}
+
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(   
+        title: Text(widget.title),
+      ),
+      body: 
+      
+        ListView(
+          children: const <Widget>[
+            Navbar(),
+            BeritaUtama(),
+            BeritaKecil(),
+            BeritaKecil(),
+            BeritaKecil(),
+            
+
+
+            ///BERITA ATAS
+            // Container(
+            //   padding: const EdgeInsets.all(3.5),
+            //   margin: const EdgeInsets.all(15),
+            //   height: 20.5,
+            //   child:
+            //     Row(
+            //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //       children: const <Widget>[ 
+            //       Text("BERITA TERBARU"),     
+            //       Text("PERTANDINGAN HARI INI"),
+            //       ],
+            //     ),
+            // ),
+            
+
+
+            ////BERITA UTAMA
+            // Container(
+            //   margin: const EdgeInsets.all(10.0),
+            //   decoration: BoxDecoration(
+            //     border: Border.all(color: Colors.purple)
+            //   ),
+            //   child: 
+            //   Column(
+            //     children: <Widget>[
+            //       Container(         
+            //         height: 150,          
+            //         decoration: const BoxDecoration(               
+            //             image: DecorationImage(
+            //               alignment: Alignment.topCenter,
+            //                fit: BoxFit.fitWidth,
+            //               image: AssetImage('assets/images/bola.jpeg'),  
+            //             ),
+            //         ),
+            //       ),
+
+            //       Container(
+            //         margin: const EdgeInsets.all(10.0),
+            //         child: const Text("Costa Mendekat ke Palmeiras",
+            //         style: TextStyle(
+            //           fontSize:20,
+            //               color:Colors.black,
+            //               fontWeight: FontWeight.bold),
+            //         ),
+            //       ), 
+
+            //       Container(
+            //       padding: const EdgeInsets.all(20.0),
+            //       alignment: Alignment.centerLeft,
+            //       color: Colors.purpleAccent,
+            //       child: const Text("Transfer"),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+
+
+
+
+            ////BERITA KECIL
+            // Container(
+            //   margin: const EdgeInsets.only(left: 10.0, right: 10.0,),
+            //   decoration: BoxDecoration(
+            //     border: Border.all(color: Colors.grey)
+            //   ),
+            //   child: 
+            //   Column(
+            //     children: <Widget>[
+            //       Row(
+            //         children: <Widget>[
+            //           Container(
+
+            //             width: 170,
+            //             height: 100, 
+            //             decoration: BoxDecoration(  
+            //               border: Border.all(color: Colors.grey),             
+            //                 image: const DecorationImage(
+            //                   alignment: Alignment.center,
+            //                   fit: BoxFit.fitWidth,
+            //                   image: AssetImage('assets/images/1.jpeg'),  
+            //                 ),                    
+            //             ),
+            //           ),
+            //           Expanded(
+            //              child : Container(
+            //               padding: const EdgeInsets.all(5.0),
+            //               width: 168,
+            //               height: 100, 
+            //               alignment: Alignment.center,
+            //             decoration: BoxDecoration(
+            //         border: Border.all(color: Colors.grey),
+            //       ),
+            //             child: const Text("Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat", 
+            //             textAlign: TextAlign.center,style: TextStyle(fontSize: 11)),
+            //           ),
+            //           )
+            //         ],
+                    
+            //       ),
+
+            //       Container(
+            //         height: 30,
+            //         alignment: Alignment.centerLeft,
+            //         width: double.infinity,
+            //           decoration: BoxDecoration(
+            //             border: Border.all(color: Colors.grey),
+            //           ),
+            //         child: const Text("Barcelona Feb 13, 2021",
+            //         style: TextStyle(fontSize: 11)
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+
+          ]
         )
     );
   }
 }
+
+
